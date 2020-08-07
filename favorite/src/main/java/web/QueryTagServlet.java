ae32a1660bb782f51b929c507498218780a45456
13631 <13631@DESKTOP-3M75H1Q>
2020-08-07 10:07:44 +0800

    实现查询所有的分类

---
+++ favorite/src/main/java/web/QueryTagServlet.java
@@ -0,0 +1,36 @@
+package web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.google.gson.Gson;
+
+import dao.TagMapper;
+import util.MyBatisHelper;
+ 
+public class QueryTagServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+      
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+ 		
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		SqlSession session = MyBatisHelper.openSession();
+	    TagMapper tm=session.getMapper(TagMapper.class);
+        Gson gson=new Gson();
+        String json=gson.toJson(tm.selectAll());
+		response.getWriter().append("Served at: ").append(json);
+	}
+
+	 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 
+		doGet(request, response);
+	}
+
+}
