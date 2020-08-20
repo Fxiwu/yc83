e0d177b5affc4dceceebcf869456bd9abd9f2889
13631 <13631@DESKTOP-3M75H1Q>
2020-08-20 09:54:47 +0800

    springboot-helloworld

---
+++ C83-pfst-SpringBoot/src/main/java/com/yc/C83pfstSpringBoot/web/Hello.java
@@ -0,0 +1,14 @@
+package com.yc.C83pfstSpringBoot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+	@GetMapping("hello")
+	public String hello() {
+		return "hello world";
+	}
+}
