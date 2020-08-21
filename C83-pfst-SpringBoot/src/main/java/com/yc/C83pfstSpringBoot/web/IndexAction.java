3cc9a85976602c7e8d9dd5c2fa615b3b3da74367
13631 <13631@DESKTOP-3M75H1Q>
2020-08-21 12:02:18 +0800

    商品详情界面+引入子页面

--- C83-pfst-SpringBoot/src/main/java/com/yc/C83pfstSpringBoot/web/IndexAction.java
+++ C83-pfst-SpringBoot/src/main/java/com/yc/C83pfstSpringBoot/web/IndexAction.java
@@ -1,11 +1,16 @@
 package com.yc.C83pfstSpringBoot.web;
 
+import java.util.List;
+import java.util.Map;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.yc.C83pfstSpringBoot.dao.ProductMapper;
+import com.yc.damai.bean.DmProduct;
 
 @Controller
 public class IndexAction {
@@ -14,7 +19,26 @@ public class IndexAction {
 	 ProductMapper pm;
 	 
 	@GetMapping("index.do")
-	public String index() {
+	public String index(Model m) {
+		//查询热销商品
+		List<DmProduct> hlist=pm.selectByHot();
+		
+		//查询最新商品
+				List<DmProduct> Llist=pm.selectByLeast();
+		//推送给页面
+		m.addAttribute("hlist",hlist);
+		m.addAttribute("Llist",Llist);
 		return "index";
+	}
+	@GetMapping("detail.do")
+	public String detail(Model m,int id) {
+		//查询商品详情
+		DmProduct dp=pm.selectById(id);
+		
+		 
+		//推送给页面
+		m.addAttribute("dp",dp);
+	 
+		 return "detail";
 	}
 }
