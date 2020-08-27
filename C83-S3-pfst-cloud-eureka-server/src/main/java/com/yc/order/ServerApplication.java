3380ca1eb94afd607877c83db1f8871d252ab5e0
13631 <13631@DESKTOP-3M75H1Q>
2020-08-27 17:22:49 +0800

    配置Eureka服务器

--- C83-S3-pfst-cloud-eureka-server/src/main/java/com/yc/order/ServerApplication.java
+++ C83-S3-pfst-cloud-eureka-server/src/main/java/com/yc/order/ServerApplication.java
@@ -2,22 +2,19 @@ package com.yc.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableEurekaServer
 @SpringBootApplication
  
-public class OrderApplication implements WebMvcConfigurer{
+public class ServerApplication implements WebMvcConfigurer{
  
 
 	public static void main(String[] args) {
-		SpringApplication.run(OrderApplication.class, args);
+		SpringApplication.run(ServerApplication.class, args);
 	}
-	//定义RestTemplate Bean
-	@Bean 
-	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
-	}
-
+	 
 }
