e0f546b19e66c28f4c7bd961b708d28f30318045
13631 <13631@DESKTOP-3M75H1Q>
2020-08-27 16:54:13 +0800

    服务远程互访

--- C83-S3-pfst-cloud-user/src/main/java/com/yc/user/UserApplication.java
+++ C83-S3-pfst-cloud-user/src/main/java/com/yc/user/UserApplication.java
@@ -2,6 +2,8 @@ package com.yc.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
  
@@ -12,5 +14,11 @@ public class UserApplication implements WebMvcConfigurer{
 
 		public static void main(String[] args) {
 			SpringApplication.run(UserApplication.class, args);
+		}
+		
+		//定义RestTemplate Bean
+		@Bean 
+		public RestTemplate getRestTemplate() {
+			return new RestTemplate();
 		}
 }
