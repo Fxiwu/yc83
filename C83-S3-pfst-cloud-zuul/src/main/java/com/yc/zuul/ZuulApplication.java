cef5368f273abcb30f452a97c3ab6d43e38011aa
13631 <13631@DESKTOP-3M75H1Q>
2020-08-29 19:29:28 +0800

    呃呃呃

--- C83-S3-pfst-cloud-zuul/src/main/java/com/yc/zuul/ZuulApplication.java
+++ C83-S3-pfst-cloud-zuul/src/main/java/com/yc/zuul/ZuulApplication.java
@@ -1,29 +1,25 @@
-package com.yc.order;
+package com.yc.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+//zuul网关注解
+@EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
-//开启声明式服务调用
-@EnableFeignClients
-//服务降级注解
-@EnableCircuitBreaker
-//开启Redis会话共享
-@EnableRedisHttpSession
-public class OrderApplication implements WebMvcConfigurer{
+ 
+public class ZuulApplication implements WebMvcConfigurer{
  
 
 	public static void main(String[] args) {
-		SpringApplication.run(OrderApplication.class, args);
+		SpringApplication.run(ZuulApplication.class, args);
 	}
 	//定义RestTemplate Bean
 	@LoadBalanced
